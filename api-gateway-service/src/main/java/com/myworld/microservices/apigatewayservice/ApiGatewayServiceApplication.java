832bdc593dd2417b64526e6cdc34547f7c7ab207
Jegatheesh <jegatheesh93@gmail.com>
2019-07-19 14:04:31 +0400

    Sleuth Integration:  - Request Tracking process  - Will assign unique id for each request

--- api-gateway-service/src/main/java/com/myworld/microservices/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/myworld/microservices/apigatewayservice/ApiGatewayServiceApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,6 +15,15 @@ public class ApiGatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+	
+	/**
+	 * To track Request details
+	 * 
+	 */
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
