5e5377ee34d42f70dc6f63f10ff72b9bb5b4a69c
Jegatheesh <jegatheesh93@gmail.com>
2019-08-02 12:06:55 +0400

    Enabled Springfox Swagger2 documentation library in Item-stock-service microservice

--- item-stock-service/src/main/java/com/myworld/microservices/itemstockservice/ItemStockServiceApplication.java
+++ item-stock-service/src/main/java/com/myworld/microservices/itemstockservice/ItemStockServiceApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableSwagger2
 public class ItemStockServiceApplication {
 
 	public static void main(String[] args) {
