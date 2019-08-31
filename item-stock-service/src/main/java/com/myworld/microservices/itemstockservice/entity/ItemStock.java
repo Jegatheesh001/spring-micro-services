06bff5fa16ea8305f45dc7293c48764d1986c1d4
Jegatheesh <jegatheesh93@gmail.com>
2019-08-31 08:53:09 +0400

    Implemented Swagger Annotations for Model

--- item-stock-service/src/main/java/com/myworld/microservices/itemstockservice/entity/ItemStock.java
+++ item-stock-service/src/main/java/com/myworld/microservices/itemstockservice/entity/ItemStock.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -16,12 +18,14 @@ import lombok.Data;
  */
 @Entity
 @Data
+@ApiModel(description = "Information About Item Stock Details")
 public class ItemStock {
 
 	@Id
 	@GeneratedValue
 	private Integer stockId;
 	private Integer batch;
+	@ApiModelProperty(notes = "Item Barcode details")
 	private String barcode;
 	private Double quantity;
 
